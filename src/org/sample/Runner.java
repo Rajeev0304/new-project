a533cd6004838cda15e0c9468c79dac57b097d04
Rajeev0304 <rajiv.aero44@gmail.com>
2021-04-29 14:05:07 +0530

    Done

---
+++ src/org/sample/Runner.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Runner {
+	public static void main(String[] args) {
+		int a=1;
+		if (a==10) {
+			System.out.println("Value is correct");
+			
+		}
+		else {
+			System.out.println("value is not correct");
+		}
+	}
+
+}
